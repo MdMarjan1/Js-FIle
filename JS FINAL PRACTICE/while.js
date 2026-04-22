1cb2150d3d11954b390e37c032144ee0e467dce4
Marjan <mdmarjan.pg@gmail.com>
2026-04-22 22:21:12 +0600

    frist wile loop problem done

---
+++ JS FINAL PRACTICE/while.js
@@ -0,0 +1,8 @@
+// "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+
+let timeSpend = 1
+while(timeSpend <= 60){
+    console.log(timeSpend,'I will invest at least 6 hrs every single day for next 60 days!');
+    timeSpend++
+}
