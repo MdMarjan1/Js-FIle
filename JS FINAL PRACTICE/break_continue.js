acda65987af80409814585f2005e0ac9d8ad6e9d
Marjan <mdmarjan.pg@gmail.com>
2026-04-25 10:04:29 +0600

    for of loop

--- JS FINAL PRACTICE/break_continue.js
+++ JS FINAL PRACTICE/break_continue.js
@@ -4,6 +4,13 @@ while(n1 < 15){
     if(n1%5 !== 0){
        continue
     }
-    console.log(n1);
+   //  console.log(n1);
    
 } 
+
+// array
+let a69 = ['apple', 'banana', 'Orange', 'Dragon', 'Gowaba']
+for(const fruit of a69){
+   console.log(fruit);
+}
+
