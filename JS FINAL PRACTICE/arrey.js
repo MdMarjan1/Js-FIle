235987c2018b75d5b1d5f4727af8648b4a7fd73a
Marjan <mdmarjan.pg@gmail.com>
2026-04-28 13:19:56 +0600

    arrey fill push

---
+++ JS FINAL PRACTICE/arrey.js
@@ -0,0 +1,13 @@
+/*
+Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+*/
+let fol=['apple','mango','banana','orange','dragon']
+let fruit=fol[3]
+console.log(fruit);
+fol[2]='jambura'
+console.log(fol);
+
