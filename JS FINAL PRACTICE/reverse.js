93d49f4decf4e8bf3d1abea7e72582f936548b94
Marjan <mdmarjan.pg@gmail.com>
2026-05-06 00:47:33 +0600

    string done

---
+++ JS FINAL PRACTICE/reverse.js
@@ -0,0 +1,10 @@
+let a0 = ''
+let a1 = 'I Love Web Development &  Programming'
+for(const re of a1){
+    a0 = re + a0
+}
+ console.log(a0);
+
+let a23 = 'Messi is not good Player'
+let a24 = a23.split(' ').reverse().join(' ')
+console.log(a24);
